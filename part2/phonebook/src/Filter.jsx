e193e499bcdef1fe3fd468c1c7ef499200856311
Lei Chun <136141200+YanxiangDu2023@users.noreply.github.com>
2025-01-07 00:10:11 +0100

    Delete part2/phonebook directory

--- part2/phonebook/src/Filter.jsx
+++
@@ -1,8 +0,0 @@
-const Filter = ({ filter, handleFilterChange }) => (
-    <div>
-      filter shown with <input value={filter} onChange={handleFilterChange} />
-    </div>
-  );
-  
-  export default Filter;
-  
